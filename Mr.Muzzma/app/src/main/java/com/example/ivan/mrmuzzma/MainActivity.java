f9b4e2bf4c1c4729e0d2f01c57e78847ad49f37b
Ivancgik <Ivan.Nikulin.2002@gmail.com>
2018-01-27 00:15:53 +0300

    Add first screen

--- Mr.Muzzma/app/src/main/java/com/example/ivan/mrmuzzma/MainActivity.java
+++ Mr.Muzzma/app/src/main/java/com/example/ivan/mrmuzzma/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.ivan.mrmuzzma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
+            @Override
+            public void onSwipeRight() {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.kizaru));
+                super.onSwipeRight();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.kizaru2));
+                super.onSwipeLeft();
+            }
+
+        });
     }
+
 }
