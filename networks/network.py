e4e0504e532a6cd4d4bb355d880167a3e54f5bf6
deniskamazur <denismazur8@gmail.com>
2018-01-28 01:20:26 +0300

    add tf network

--- networks/network.py
+++ networks/network.py
@@ -1,129 +1,128 @@
-import lasagne
-from lasagne.layers import InputLayer, Conv1DLayer as ConvLayer, get_output
-from lasagne.utils import floatX
-
-import theano
-import theano.tensor as T
-
+import tensorflow as tf
+import librosa
 import numpy as np
 
-from scipy import optimize
 
-import librosa
-
-from util import read_audio_spectum
-
-
+N_FFT = 2048
+N_FILTERS = 4096
 N_CHAN = 1025
 N_SAMPLES = 430
-N_FILTERS = 4096
-STYLE_ALPHA = 1e-2
-N_FFT = 2048
+ALPHA= 1e-2
+learning_rate= 1e-3
+iterations = 100
 
 
 class AudioNet:
-    def __init__(self, n_chan=N_CHAN, n_samples=N_SAMPLES, n_filters=N_FILTERS, style_alpha=STYLE_ALPHA):
-        """
-        Build an AuidNet instance
-        :param n_chan: number of channels
-        :param n_samples: number of samples
-        """
+
+    def __init__(self, n_chan=N_CHAN, n_filters=N_FILTERS, n_samp=N_SAMPLES):
         self.n_chan = n_chan
-        self.n_samples = n_samples
-        self.n_filt = n_filters
+        self.n_samp = n_samp
+        self.n_filters = n_filters
 
-        self.content_input = T.tensor3("input content")
-        self.style_input = T.tensor3("input style")
-        self.generated = T.tensor3("generated image")
+        std = np.sqrt(2) * np.sqrt(2.0 / ((self.n_chan + self.n_filters) * 11))
+        self.kernel = np.random.randn(1, 11, self.n_chan, self.n_filters) * std
 
-        self.net = self._build_net()
+    def transfer_style(self, content_file, style_file, output_file="out.mp3"):
+        a_content, fs = read_audio_spectum(content_file)
+        a_style, fs = read_audio_spectum(style_file)
 
-        self.content_features = get_output(self.net.conv, {self.net.inputs: self.content_input})
-        self.style_features = get_output(self.net.conv, {self.net.inputs: self.style_input})
-        self.gen_features = get_output(self.net.conv, {self.net.inputs: self.generated})
+        a_content_tf = np.ascontiguousarray(a_content.T[None, None, :, :])
+        a_style_tf = np.ascontiguousarray(a_style.T[None, None, :, :])
 
-        loss = style_loss(self.style_features, self.gen_features) + \
-            STYLE_ALPHA * content_loss(self.content_features, self.gen_features)
+        content_features, style_gram = self._compute_features(a_content_tf, a_style_tf)
 
-        grad = T.grad(loss, self.generated)
+        res = self._optimize(content_features, style_gram)
 
-        self.loss_func = theano.function([self.generated, self.style_input, self.content_input], loss)
-        self.grad_func = theano.function([self.generated, self.style_input, self.content_input], grad)
+        x = self._invert_spectrogram(a_content, res)
+        librosa.output.write_wav(output_file, x, fs)
 
-    def _build_net(self):
-        class net:
-            inputs = InputLayer((1, self.n_chan, self.n_samples))
-            conv = ConvLayer(inputs, self.n_filt, 11, W=lasagne.init.GlorotNormal(gain='relu'))
+    def _compute_features(self, content, style):
+        g = tf.Graph()
+        with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
+            # data shape is "[batch, in_height, in_width, in_channels]",
+            x = tf.placeholder('float32', [1, 1, self.n_samp, self.n_chan], name="x")
 
-        return net
+            kernel_tf = tf.constant(self.kernel, name="kernel", dtype='float32')
+            conv = tf.nn.conv2d(
+                x,
+                kernel_tf,
+                strides=[1, 1, 1, 1],
+                padding="VALID",
+                name="conv")
 
-    def _optimize(self, pic, style):
-        t = floatX(np.zeros((1, self.n_chan, self.n_samples)))
-        content = pic
+            net = tf.nn.relu(conv)
 
-        eval_loss = EvalFunc(style, content, self.loss_func, self.n_chan, self.n_samples)
-        eval_grad = EvalFunc(style, content, self.grad_func, self.n_chan, self.n_samples)
+            content_features = net.eval(feed_dict={x: content})
+            style_features = net.eval(feed_dict={x: style})
 
-        res = optimize.fmin_l_bfgs_b(eval_loss, t.flatten(), fprime=eval_grad, maxfun=500)
-        t = res[0].reshape((1, self.n_chan, self.n_samples))
+            features = np.reshape(style_features, (-1, self.n_filters))
+            style_gram = np.matmul(features.T, features) / self.n_samp
 
-        a = np.zeros_like(pic[0])
-        a[:self.n_chan, :] = np.exp(t[0]) - 1
+        return content_features, style_gram
 
+    def _optimize(self, content_features, style_gram):
+        with tf.Graph().as_default():
+            # Build graph with variable input
+            #     x = tf.Variable(np.zeros([1,1,N_SAMPLES,N_CHANNELS], dtype=np.float32), name="x")
+            x = tf.Variable(np.random.randn(1, 1, self.n_samp, self.n_chan).astype(np.float32) * 1e-3, name="x")
+
+            kernel_tf = tf.constant(self.kernel, name="kernel", dtype='float32')
+            conv = tf.nn.conv2d(
+                x,
+                kernel_tf,
+                strides=[1, 1, 1, 1],
+                padding="VALID",
+                name="conv")
+
+            net = tf.nn.relu(conv)
+
+            content_loss = ALPHA * 2 * tf.nn.l2_loss(
+                net - content_features)
+
+            _, height, width, number = map(lambda i: i.value, net.get_shape())
+
+            size = height * width * number
+            feats = tf.reshape(net, (-1, number))
+            gram = tf.matmul(tf.transpose(feats), feats) / N_SAMPLES
+            style_loss = 2 * tf.nn.l2_loss(gram - style_gram)
+
+            # Overall loss
+            loss = content_loss + style_loss
+
+            opt = tf.contrib.opt.ScipyOptimizerInterface(
+                loss, method='L-BFGS-B', options={'maxiter': 300})
+
+            # Optimization
+            with tf.Session() as sess:
+                sess.run(tf.initialize_all_variables())
+
+                opt.minimize(sess)
+                result = x.eval()
+
+        return result
+
+    def _invert_spectrogram(self, content, result):
+        a = np.zeros_like(content)
+        a[:self.n_chan, :] = np.exp(result[0, 0].T) - 1
+
+        # This code is supposed to do phase reconstruction
         p = 2 * np.pi * np.random.random_sample(a.shape) - np.pi
         for i in range(500):
-            print "Iteration ", i
             S = a * np.exp(1j * p)
             x = librosa.istft(S)
             p = np.angle(librosa.stft(x, N_FFT))
 
         return x
 
-    def transfer_style(self, content_path, style_path, save_path="output.mp3"):
-        content, fs = read_audio_spectum(content_path)
-        style, fs = read_audio_spectum(style_path)
 
-        out = self._optimize(content, style)
-
-        librosa.output.write_wav(save_path, out, fs)
-
-
-
-
-class EvalFunc:
-    def __init__(self, style, content, func, n_chan, n_samples):
-        self.style = style
-        self.content = content
-        self.n_chan = n_chan
-        self.n_samples = n_samples
-        self.func = func
-
-    def __call__(self, generated):
-        generated = floatX(generated.reshape((1, self.n_chan, self.n_samples)))
-        return np.array(self.func(generated, self.style, self.content).flatten().astype('float64'))
-
-
-def gram_matrix(x):
-    g = T.tensordot(x, x, axes=([2], [2])) / x.shape[2]
-    return g
-
-def style_loss(A, X):
-    G1 = gram_matrix(A)
-    G2 = gram_matrix(X)
-    loss = ((G1 - G2)**2).sum()
-    return loss
-
-def content_loss(A, X):
-    return ((A - X)**2).sum()
+def read_audio_spectum(filename):
+    x, fs = librosa.load(filename)
+    S = librosa.stft(x, N_FFT)
+    p = np.angle(S)
+    S = np.log1p(np.abs(S[:, :430]))
+    return S, fs
 
 
 if __name__ == "__main__":
-    # Your tests here
-
-    CONTENT_PATH = "audio/imperial.mp3"
-    STYLE_PATH = "audio/futurama.mp3"
-
     net = AudioNet()
-    print "It compilled"
-
-    net.transfer_style(CONTENT_PATH, STYLE_PATH)
+    net.transfer_style("inputs/eminem.mp3", "inputs/imperial.mp3", "penis.mp3")
